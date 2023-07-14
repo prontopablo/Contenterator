7c70175b28dd2441151d27debc6cd4db257b9d91
Darragh Nolan <55544101+prontopablo@users.noreply.github.com>
2023-07-14 16:58:51 +0100

    Updated Countdown formatting and buttons

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -8,32 +8,37 @@ const Countdown = () => {
   const [selectedLetters, setSelectedLetters] = useState([]);
   const [solution, setSolution] = useState('');
   const [timer, setTimer] = useState(30);
+  const [wordList, setWordList] = useState([]);
 
   const vowels = 'AEIOU';
   const consonants = 'BCDFGHJKLMNPQRSTVWXYZ';
 
-  const handleSolve = useCallback(() => {
-    // Read the words.txt file
-    fetch('../words.txt')
-      .then(response => response.text())
-      .then(wordList => {
-        // Split the word list into an array of words
-        const validWords = wordList.split('\n');
-  
-        // Check if the selected letters form a valid word
-        const selectedWord = selectedLetters.join('').toLowerCase();
-        const isValidWord = validWords.includes(selectedWord);
-  
-        if (isValidWord) {
-          setSolution(selectedWord);
-        } else {
-          setSolution('No valid word found');
-        }
+  const loadWordList = useCallback(() => {
+    fetch('/words.txt')
+      .then((response) => response.text())
+      .then((text) => {
+        const words = text.split('\n');
+        setWordList(words);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error reading the word list:', error);
       });
-  }, [selectedLetters]);
+  }, []);
+
+  useEffect(() => {
+    loadWordList();
+  }, [loadWordList]);
+
+  const handleSolve = useCallback(() => {
+    const selectedWord = selectedLetters.join('').toLowerCase();
+    const isValidWord = wordList.includes(selectedWord);
+
+    if (isValidWord) {
+      setSolution('Word is valid');
+    } else {
+      setSolution('No valid word found, ' + selectedWord + ' is not a word.');
+    }
+  }, [selectedLetters, wordList]);
 
   useEffect(() => {
     if (letters.length === 9) {
@@ -62,23 +67,20 @@ const Countdown = () => {
 
   const handleSelectLetter = (letter) => {
     setSelectedLetters([...selectedLetters, letter]);
+    setLetters(letters.filter((l) => l !== letter));
   };
 
   const handleClearSelection = () => {
     setSelectedLetters([]);
+    setLetters([]);
   };
 
   return (
     <div className="container">
       <h1>Countdown</h1>
       <div className="clock-container">
-        <Clock
-          className="clock"
-          value={new Date(0, 0, 0, 0, 0, timer)} // Set the clock value based on the timer
-          size={150}
-          renderNumbers={true}
-        />
-        <div className="timer">{timer}</div> {/* Added timer display */}
+        <Clock className="clock" value={new Date(0, 0, 0, 0, 0, timer)} size={150} renderNumbers={true} />
+        <div className="timer">{timer}</div>
       </div>
       <div className="letters-section">
         <p>{letters.join(' ')}</p>
