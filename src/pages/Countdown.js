38761d68356e45ce1bcf48ebd91121f7e94ed20c
Darragh Nolan <55544101+prontopablo@users.noreply.github.com>
2023-07-14 11:43:00 +0100

    Added Countdown clock, started word verification, updated styling for home/countdown pages

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -1,16 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import Clock from 'react-clock';
+import 'react-clock/dist/Clock.css';
+import './Countdown.css';
 
 const Countdown = () => {
   const [letters, setLetters] = useState([]);
   const [selectedLetters, setSelectedLetters] = useState([]);
   const [solution, setSolution] = useState('');
+  const [timer, setTimer] = useState(30);
 
   const vowels = 'AEIOU';
   const consonants = 'BCDFGHJKLMNPQRSTVWXYZ';
 
+  const handleSolve = useCallback(() => {
+    // Read the words.txt file
+    fetch('../words.txt')
+      .then(response => response.text())
+      .then(wordList => {
+        // Split the word list into an array of words
+        const validWords = wordList.split('\n');
+  
+        // Check if the selected letters form a valid word
+        const selectedWord = selectedLetters.join('').toLowerCase();
+        const isValidWord = validWords.includes(selectedWord);
+  
+        if (isValidWord) {
+          setSolution(selectedWord);
+        } else {
+          setSolution('No valid word found');
+        }
+      })
+      .catch(error => {
+        console.error('Error reading the word list:', error);
+      });
+  }, [selectedLetters]);
+
+  useEffect(() => {
+    if (letters.length === 9) {
+      const interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+
+      if (timer === 0) {
+        clearInterval(interval);
+        handleSolve();
+      }
+
+      return () => clearInterval(interval);
+    }
+  }, [letters, timer, handleSolve]);
+
   const handleGenerateLetter = (type) => {
     if (letters.length >= 9) {
-      return; // Maximum number of letters reached, exit the function
+      return;
     }
 
     const availableLetters = type === 'vowel' ? vowels : consonants;
@@ -26,17 +68,23 @@ const Countdown = () => {
     setSelectedLetters([]);
   };
 
-  const handleSolve = () => {
-    // Implement your logic to find the solution here
-    // Assign the solution to the 'solution' state variable
-  };
-
   return (
-    <div>
+    <div className="container">
       <h1>Countdown</h1>
-      <p>Letters: {letters.join(' ')}</p>
-      <p>Selected Letters: {selectedLetters.join(' ')}</p>
-      <p>Solution: {solution}</p>
+      <div className="clock-container">
+        <Clock
+          className="clock"
+          value={new Date(0, 0, 0, 0, 0, timer)} // Set the clock value based on the timer
+          size={150}
+          renderNumbers={true}
+        />
+        <div className="timer">{timer}</div> {/* Added timer display */}
+      </div>
+      <div className="letters-section">
+        <p>{letters.join(' ')}</p>
+        <p>Selected Letters: {selectedLetters.join(' ')}</p>
+        <p>Solution: {solution}</p>
+      </div>
       <button onClick={() => handleGenerateLetter('vowel')} disabled={letters.length >= 9}>
         Add Vowel
       </button>
@@ -46,7 +94,6 @@ const Countdown = () => {
       <button onClick={handleClearSelection}>Clear Selection</button>
       <button onClick={handleSolve}>Solve</button>
       <div>
-        {/* Render the letters as buttons */}
         {letters.map((letter, index) => (
           <button key={index} onClick={() => handleSelectLetter(letter)}>
             {letter}
